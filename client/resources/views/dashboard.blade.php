6b8e04c83a26133ec2dc6cb7bb3eb9e70e711f28
Vincy Deguma <vinsmar8888@gmail.com>
2024-06-24 14:53:22 +0800

    main

--- client/resources/views/dashboard.blade.php
+++ client/resources/views/dashboard.blade.php
@@ -5,17 +5,14 @@
         </h2>
     </x-slot>
 
-    @if (Session::has('success'))
-    <div class="alert alert-success bg-green-800 text-white w-[20em] m-3 p-3 rounded">
-        <ul>
-            <li>{{ Session::get('success') }}</li>
-        </ul>
-    </div>
-    @endif
-
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @if (Session::has('success'))
+            <div class="alert alert-success bg-green-800 text-white min-w-[100%] mb-3 p-3 rounded">
+                <h3>{{ Session::get('success') }}</h3>
+            </div>
+        @endif
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <x-nav-link href="/redirect">
