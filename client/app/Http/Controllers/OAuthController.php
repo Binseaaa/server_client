50efa72460249b19f17df64f1122885ed30ebb7e
Vincy Deguma <vinsmar8888@gmail.com>
2024-06-24 14:26:20 +0800

    main

--- client/app/Http/Controllers/OAuthController.php
+++ client/app/Http/Controllers/OAuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 
 class OAuthController extends Controller
 {
@@ -17,7 +19,20 @@ class OAuthController extends Controller
     }
 
     public function callback(Request $request) {
-        $code = $request->input('code');
-        return response()->json(['code' => $code]);
+        $response = Http::post("http://127.0.0.1:8001/oauth/token", [
+            'grant_type' => 'authorization_code',
+            'client_id' => '4',
+            'client_secret' => 'Cu0sptPLE2WJReBCIBXyg0bS4V0rb1PiITODZBy1',
+            'redirect_uri' => 'http://127.0.0.1:8000/callback',
+            'code' => $request->code
+        ]);
+        $response = $response->json();
+
+        $request->user()->token()->delete();
+        $request->user()->token()->create([
+            'access_token' => $response['access_token']
+        ]);
+
+        return redirect('/dashboard')->with('success', 'Authorization Successful');
     }
 }
--- client/resources/views/dashboard.blade.php
+++ client/resources/views/dashboard.blade.php
@@ -5,6 +5,15 @@
         </h2>
     </x-slot>
 
+    @if (Session::has('success'))
+    <div class="alert alert-success bg-green-800 text-white w-[20em] m-3 p-3 rounded">
+        <ul>
+            <li>{{ Session::get('success') }}</li>
+        </ul>
+    </div>
+    @endif
+
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
